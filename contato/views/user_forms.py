aa4dc34781cc6a5dafdbf7d52cc4195bb18fe262
LucasCarneiro12 <dsousalucas2005@gmail.com>
2024-05-08 23:45:23 -0300

    forms de atualização - user/update

--- contato/views/user_forms.py
+++ contato/views/user_forms.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from contato.forms import RegisterForm
+from django.contrib import messages, auth
+from contato.forms import RegisterForm, ResgisterUpdateForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     form = RegisterForm()
@@ -24,3 +25,56 @@ def register(request):
             'form': form
         }
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+           # messages.success('logado com sucesso')
+            print(user)
+            auth.login(request, user)
+            return redirect('contato:index')
+        else:
+            messages.error(request, 'login inválido')
+
+    return render(
+        request, 
+        'contato/login.html',
+        {
+            'form': form
+        }
+    )
+
+def user_update(request):
+     form = ResgisterUpdateForm(instance=request.user)
+
+     if request.method != 'POST':
+         
+        return render(
+            request,
+            'contato/user_update.html',
+            {
+                'form': form
+            }
+        )
+     form = ResgisterUpdateForm(data=request.POST, instance=request.user)
+
+     if not form.is_valid():
+         return render(
+            request,
+            'contato/user_update.html',
+            {
+                'form': form
+            }
+        )
+     form.save()
+     return redirect('contato:user_update')
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contato:login')
